fe48c30dfef50191bdf962e64109d07f8734eb0b
Ninespring <xiaolongshen.stuff@gmail.com>
2015-06-25 16:37:02 +0800

    Add pickle-lized classifier into project. For AEON usage.

--- sdk_utility.py
+++ sdk_utility.py
@@ -9,8 +9,32 @@ ADD-ON FUNCTION NEED TO BE FILLED.
 '''
 
 
+class ClfDataStorage:
+	def __init__(self):
+		self.wifi_matrix = None
+		self.outlier_mac_count = None
+		self.hit_mac_count = None
+		self.total_mac_count = None
+
+
 class SDKUtility:
 	def __init__(self):
 		pass
+	"""
+	FUNCTION:
+		EXTRACT_WIFI    EXTRACTING WIFI FILE ONLY ACCORDING TO REFERENCE LIST FOR CLASSIFICATION USAGE
+	"""
+	def extract_wifi(self, wifi_path, ref_list=[]):
+		"""
+		:param wifi_path:
+		:param ref_list:
+		:return:
+			miss_count
+			hit_count
+			wifi_matrix
+		"""
+		if ref_list:
 
-	def
+		else:
+			print "No Reference List Found"
+			return
